f84e479b01331c73355d11a493dbe2c8c8ee8d32
xang1234 <davidten7@gmail.com>
2018-08-29 00:36:43 +0800

    create cluster_subgraph_for_clustering to split clustering function

--- code/main_prog.py
+++ code/main_prog.py
@@ -33,13 +33,17 @@ G_connected=max(nx.connected_component_subgraphs(G), key=len)
 
 # Select Attribute for weight
 jac=nx.get_edge_attributes(G_connected,'Jacquard')
-jac_exp={k:np.exp(5*v) for (k,v) in jac.items()}
+jac_exp={k:np.exp(v) for (k,v) in jac.items()}
 nx.set_edge_attributes(G_connected,jac_exp,'weight' )
 
 # Louvain Clustering
-Cluster_Louvain=f.cluster_subgraph_by_year_louvain(G_connected,option='accumulate',
-                                     connected='yes',retain_clus='no',
-                                     res_louv=5,wei='weight') 
+retain='yes'
+H=f.cluster_subgraph_for_clustering(G_connected,option='accumulate',
+                                     connected='yes',retain_clus=retain)
+
+Cluster_Louvain=f.cluster_subgraph_by_year_louvain(G_connected, H,
+                                                   retain_clus=retain,
+                                                   res_louv=5,wei='Year_Diff') 
 
 Cluster_CNM=f.cluster_subgraph_by_year_cnm(G_connected,option='accumulate',
                                      connected='yes')
@@ -54,7 +58,7 @@ pagerank=nx.pagerank(Cluster_Louvain)
 top50_pagerank = sorted(pagerank.items(), key=operator.itemgetter(1), reverse=True)[:50]
 
 # Get paper clusters
-plot=[f.get_papercluster(Cluster_Louvain,top50_pagerank[i][0],Louvain) for i in range(len(top50_pagerank))]
+plot=[f.get_papercluster(Cluster_Louvain,top50_pagerank[i][0],Louvain)[1] for i in range(len(top50_pagerank))]
 
 for i in range(len(plot)):
     plt.plot(plot[i])
